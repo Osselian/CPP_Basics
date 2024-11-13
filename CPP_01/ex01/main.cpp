ad63ee6c1fc0f38d59a5e52aafe700896de5775e
Osselian <rossemir11@gmail.com>
2024-11-13 11:43:04 +0400

    warning add

--- CPP_01/ex01/main.cpp
+++ CPP_01/ex01/main.cpp
@@ -2,40 +2,53 @@
 #include <climits>
 #include <iostream>
 
+void announceAll(int N, Zombie *horde)
+{
+	if (horde)
+	{
+		for (int i = 0; i < N; i++)
+			horde[i].announce();
+    }
+}
+
 int main(void)
 {
 	int N = 1;
 
 	std::cout << "N is " << N << std::endl;
 	Zombie *horde = zombieHorde(N,"Marvin");
-	for (int i = 0; i < N; i++)
-		horde[i].announce();
+	announceAll(N, horde);
 
 	delete[] horde ;
 	
 	N = 5;
 	std::cout << "N is " << N << std::endl;
 	horde = zombieHorde(N,"Marvin");
-	for (int i = 0; i < N; i++)
-		horde[i].announce();
+	announceAll(N, horde);
 
 	delete[] horde ;
 
 	N = -5;
 	std::cout << "N is " << N << std::endl;
 	horde = zombieHorde(N,"Marvin");
-	for (int i = 0; i < N; i++)
-		horde[i].announce();
+	announceAll(N, horde);
 
 	delete[] horde ;
 
-	N = INT_MAX / 4;
+	N = INT_MAX / 8;
 	std::cout << "N is " << N << std::endl;
 	horde = zombieHorde(N,"Marvin");
-	for (int i = 0; i < N; i++)
-		horde[i].announce();
+	announceAll(N, horde);
 
 	delete[] horde ;
 
+	N = INT_MIN;
+	std::cout << "N is " << N << std::endl;
+	horde = zombieHorde(N,"Marvin");
+	announceAll(N, horde);
+
+	delete[] horde ;
+
+	std::cout << "Bye!" << std::endl;
 	return (0);
 }
--- CPP_01/ex01/zombieHorde.cpp
+++ CPP_01/ex01/zombieHorde.cpp
@@ -2,6 +2,26 @@
 #include <climits>
 #include <iostream>
 
+bool handleSignificantInput(int N)
+{
+	std::string input;
+
+	std::cout << "WARNING: you are going to create " << N
+	          << " size horde! Memory usage may be critical! Do "
+	             "you want to proceed? yes/no"
+	          << std::endl;
+	while (1) 
+	{
+		std::cin >> input;
+		if (std::cin.eof() || input.compare("no") == 0)
+			return false;
+		if (input.compare("yes") == 0)
+			break;
+		std::cout << "Please, print \"yes\" or \"no\"" << std::endl;
+	}
+	return true;
+}
+
 Zombie *zombieHorde(int N, std::string name)
 {
 	Zombie *newHorde;
@@ -11,14 +31,12 @@ Zombie *zombieHorde(int N, std::string name)
 		std::cout << "A valid positive number required to create a horde!" << std::endl;
 		return NULL;
 	}
-	if (N > INT_MAX)
-	{
-		std::cout << "WARNING: you are going to create " << N << " size horde! Memory usage may be critical!" << std::endl;
+	if (N >= INT_MAX / 8 && !handleSignificantInput(N))
 		return NULL;
-	}
-	newHorde = new Zombie[N];
-	for (int i = 0; i < N; i++)
-		newHorde[i].setName(name);
 
-	return newHorde;
+	newHorde = new Zombie[N];
+    for (int i = 0; i < N; i++)
+      newHorde[i].setName(name);
+
+    return newHorde;
 }
