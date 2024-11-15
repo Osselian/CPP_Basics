b911050de87350f571eed24a9e494a726d7cc423
Osselian <rossemir11@gmail.com>
2024-11-15 16:50:36 +0400

    add test cases

--- CPP_02/ex00/main.cpp
+++ CPP_02/ex00/main.cpp
@@ -11,5 +11,13 @@ int main(void)
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
 
+	a.setRawBits(1);
+	b.setRawBits(2);
+	c.setRawBits(3);
+
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+
 	return (0);
 }
--- CPP_02/ex02/main.cpp
+++ CPP_02/ex02/main.cpp
@@ -4,25 +4,44 @@
 
 int main(void)
 {
-	Fixed a;
+	Fixed a(1);
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-
-	std::cout << b << std::endl;
-
-	std::cout << Fixed::max( a, b ) << std::endl;
-	
-	std::cout << (a > b) << std::endl;
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+	std::cout << "Bools:" << std::endl;
+	std::cout << "a > b: " << (a > b) << std::endl;
+	std::cout << "a < b: " << (a < b) << std::endl;
+	std::cout << "a >= b: " << (a >= b) << std::endl;
+	std::cout << "a <= b: " << (a <= b) << std::endl;
+	std::cout << "a != b: " << (a != b) << std::endl;
+	std::cout << "a == b: " << (a == b) << std::endl << std::endl;
 
 	std::cout << "b + a = " << (b + a) << std::endl;
 	std::cout << "b - a = " << (b - a) << std::endl;
 	std::cout << "b * a = " << (b * a) << std::endl;
-	std::cout << "b / a = " << (b / a) << std::endl;
+	std::cout << "b / a = " << (b / a) << std::endl << std::endl;
 
+	std::cout << "Increment/Decrement on a:" << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "++a: " << ++a << std::endl;
+	std::cout << "a: " << a << std::endl;
+	std::cout << "a++: " << a++ << std::endl;
+	std::cout << "a: " << a << std::endl << std::endl;
+
+	std::cout << "b: " << b << std::endl << std::endl;
+
+	std::cout << "b + a = " << (b + a) << std::endl;
+	std::cout << "b - a = " << (b - a) << std::endl;
+	std::cout << "b * a = " << (b * a) << std::endl;
+	std::cout << "b / a = " << (b / a) << std::endl << std::endl;
+
+	std::cout << "a: " << a << ", b: " << b << std::endl;
+	std::cout << "max of a, b: " << Fixed::max( a, b ) << std::endl;
+	std::cout << "min of a, b: " << Fixed::min( a, b ) << std::endl << std::endl;
+	Fixed c(  2 );
+	std::cout << "a: " << a << ", c: " << c << std::endl;
+	std::cout << "max of a, c: " << Fixed::max( a, c ) << std::endl;
+	std::cout << "min of a, c: " << Fixed::min( a, c ) << std::endl << std::endl;
+	
 	return (0);
 }
